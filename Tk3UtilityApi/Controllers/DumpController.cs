e571d001925a33a4ef0792e0ca8b1590aeeccb20
Takayuki Hirasawa <istb16@gmail.com>
2022-03-14 22:33:40 +0900

    Dumpにホスト名を追加

--- Tk3UtilityApi/Controllers/DumpController.cs
+++ Tk3UtilityApi/Controllers/DumpController.cs
@@ -12,18 +12,23 @@ public class DumpController : BaseController
         return await sr.ReadToEndAsync();
     }
 
-    private async Task<object> GetRequestAsync() => new
-    {
-        RemoteIpAddress = Request.HttpContext.Connection.RemoteIpAddress?.ToString(),
-        LocalIpAddress = Request.HttpContext.Connection.LocalIpAddress?.ToString(),
-        Method = Request.Method,
-        ContentType = Request.ContentType,
-        Url = $"{Request.Scheme}://{Request.Host}{Request.Path}{Request.QueryString}",
-        Query = Request.Query,
-        Headers = Request.Headers.Select(h => new { h.Key, Value = h.Value.FirstOrDefault() }),
-        Cookies = Request.Cookies.Select(c => new { c.Key, Value = c.Value.FirstOrDefault() }),
-        Body = await GetBodyStringAsync(),
-    };
+    private async Task<object> GetRequestAsync() {
+        var remoteIpAddress = Request.HttpContext.Connection.RemoteIpAddress?.ToString();
+        return new
+        {
+            RemoteIpAddress = remoteIpAddress,
+            RemoteHostName = remoteIpAddress == null
+                ? null
+                : System.Net.Dns.GetHostEntry(remoteIpAddress).HostName,
+            Method = Request.Method,
+            ContentType = Request.ContentType,
+            Url = $"{Request.Scheme}://{Request.Host}{Request.Path}{Request.QueryString}",
+            Query = Request.Query,
+            Headers = Request.Headers.Select(h => new { h.Key, Value = h.Value.FirstOrDefault() }),
+            Cookies = Request.Cookies.Select(c => new { c.Key, Value = c.Value.FirstOrDefault() }),
+            Body = await GetBodyStringAsync(),
+        };
+    }
 
     [HttpGet]
     public async Task<object> Get() => await GetRequestAsync();
